cca325b29374528ee706736e408b4160371a509a
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-13 07:22:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.uoombu0.mongodb.net/?retryWrites=true&w=majority`;
+
+//middle ware
+
+app.use(cors());
+app.use(express.json());
+
+//mongodb connection
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("toyverse");
+    const toyCollection = database.collection("toys");
+
+    //api request's
+
+    app.get("/toys", async (req, res) => {
+      const result = await toyCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("WELCOME TO TOYVERSE SERVER");
+});
+
+app.listen(port, () => {
+  console.log("App running on port:", port);
+});
